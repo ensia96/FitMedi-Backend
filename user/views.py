143ea8cb53f52ab07eddabca475d6726e9994181
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-06-17 11:44:01 +0900

    made simple login

--- user/views.py
+++ user/views.py
@@ -1,4 +1,8 @@
 import json
+import bcrypt
+import jwt
+
+from my_settings import SECRET
 
 from django.views import View
 from django.http import HttpResponse, JsonResponse
@@ -7,12 +11,34 @@ from .models import User
 
 
 class UserAuthView(View):
-    def get(self, request):
-        return JsonResponse({"message": "for_test haha"}, status=200)
+    def make_token(self, user):
+        return jwt.encode(
+            {"user": user.id}, SECRET["key"], algorithm=SECRET["algorithm"]
+        )
+
+    def create_account(self, data):
+        account = User.objects.create(
+            user=data["user"],
+            password=bcrypt.hashpw(
+                data["password"].encode("utf-8"), bcrypt.gensalt()
+            ).decode("utf-8"),
+        )
+        return account
 
     def post(self, request):
         data = json.loads(request.body)
-        print(data["user"])
-        print(data["password"])
-        User.objects.get_or_create(user=data["user"], password=data["password"])
-        return JsonResponse(data, status=200)
+
+        account = User.objects.filter(user=data["user"])
+
+        if not account:
+            account = self.create_account(data)
+            token = self.make_token(account).decode("utf-8")
+            return JsonResponse({"Authorization": token}, status=201)
+
+        if bcrypt.checkpw(
+            data["password"].encode("utf-8"), account.first().password.encode("utf-8")
+        ):
+            token = self.make_token(account.first()).decode("utf-8")
+            return JsonResponse({"Authorization": token}, status=200)
+
+        return JsonResponse({"MESSAGE": "FAILED_TO_LOGIN"}, status=401)
